cd5b1548b6b2cab5ea58a8b366279105f2fa508c
Nasib Farooq <nasibfarooq@outlook.com>
2024-03-27 15:26:24 +0530

    edit

--- url_shortener/url_shortener_app/models.py
+++ url_shortener/url_shortener_app/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class URL(models.Model):
+    original_url = models.URLField(unique=True)
+    short_url = models.CharField(max_length=50, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
--- url_shortener/url_shortener_app/views.py
+++ url_shortener/url_shortener_app/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import Http404
+from .models import URL
+import string
+import random
 
-# Create your views here.
+def generate_short_url():
+    characters = string.ascii_letters + string.digits
+    short_url = ''.join(random.choices(characters, k=6))
+    return short_url
+
+def shorten_url(request):
+    if request.method == 'POST':
+        original_url = request.POST['original_url']
+        # Check if the URL already exists in the database
+        try:
+            url = URL.objects.get(original_url=original_url)
+        except URL.DoesNotExist:
+            # If the URL doesn't exist, generate a new short URL and save it
+            short_url = generate_short_url()
+            url = URL.objects.create(original_url=original_url, short_url=short_url)
+        return render(request, 'url_shortener/shortened_url.html', {'short_url': url.short_url})
+    return render(request, 'url_shortener/shorten_url.html')
+
+def redirect_to_original(request, short_url):
+    try:
+        url = URL.objects.get(short_url=short_url)
+        return redirect(url.original_url)
+    except URL.DoesNotExist:
+        raise Http404
